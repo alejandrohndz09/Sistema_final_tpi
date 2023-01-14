b45017fd1262584495ee75bc5a83dfc9ba02fad4
alejandrohndz09 <molinaflavio0@gmail.com>
2023-01-14 08:48:46 -0600

    crud completo

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,5 @@ Route::get('show-modal', 'App\Http\Controllers\MedidorController@showModal')->na
 
 Route::resource('consumo', 'App\Http\Controllers\ConsumoController');
 Route::resource('medidor', 'App\Http\Controllers\MedidorController');
-<<<<<<< HEAD
 Route::post('/medidor/update', 'App\Http\Controllers\MedidorController@update');
-=======
 Route::resource('persona', 'App\Http\Controllers\PersonaController');
->>>>>>> a92e6575721b8662c17ae1045530d7fa1c543cc9
