529a968391f641323ccb1b85da55f9f8265e09b5
alejandrohndz09 <molinaflavio0@gmail.com>
2023-08-26 22:26:35 -0600

    campio de boton

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div id="layoutSidenav_content">
-    <main>
+    <main>  
         <div class="container-fluid px-4">
             <h1 class="mt-4">Tables</h1>
             <ol class="breadcrumb mb-4">
